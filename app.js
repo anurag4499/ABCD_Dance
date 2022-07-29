db840d50064c8b17d1b2d14cb686cced989a6f79
Anurag <anuragmishra4499@gmail.com>
2022-07-29 15:37:10 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyparser = require("body-parser");
+const port = 80 ;
+
+// console.log("Hey bhai")
+//   mongoose.connect ('mongodb://0.0.0.0:27017/ContactDance',{useNewUrlParser : true , useUnifiedTopology : true } ) ;
+//  var db = mongoose.connection ;
+//   db.on ( 'error' , console.error.bind ( console,'connection error :' ) ) ; 
+//   db.once ( 'open' , function () {
+//      console.log("we are coonected !")});
+
+    //  const contactSchema = new mongoose.Schema({
+    //     name: String,
+    //     phone: String,
+    //     email: String,
+    //     address: String,
+    //     desc: String,
+
+    //   });
+
+    //   const Contact = mongoose.model('Contact', contactSchema);
+
+    // kittySchema.methods.speak = function speak() {
+    //   const greeting ="My name is " + this.name
+    //   console.log(greeting); 
+    // };
+
+
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = { }
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res)=>{
+    const params = { }
+    res.status(200).render('contact.pug', params);
+})
+app.post('/contact', (req, res)=>{
+   var myData =new Contact(req.body);
+   myData.save().then(()=>{
+     res.send("This item has been Saved to th data base");
+    }).catch(()=>{
+      res.status(400).send("item was not saved to the data base")
+    })
+})
+
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
